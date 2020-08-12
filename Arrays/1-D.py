17cecc36a62aa53b8b85c57a1514bf42668a4b39
Pathak <PATHAKABHISHEK0003@GMAIL.COM>
2020-08-12 13:46:39 +0530

    added 1-D array implementation in python

---
+++ Arrays/1-D.py
@@ -0,0 +1,20 @@
+######################## One Dimensional Arrays #########################
+
+# Declaration of Arrays/List
+arr = [3, 4, 5, 6, 10, 1]
+
+# Element at Index 2
+# print(arr[2])
+
+# Element at Index 5
+# print(arr[5])
+
+
+# Print Element One By One
+for index in range(0, len(arr)):
+    print(arr[index])
+
+print()
+# Print Element Ony By One Different Approach
+for element in arr:
+    print(element)
